0f2529db9fae0d76a9ebe8153dbf5b9c594b6e85
Shabaykina <shabaykina@ivit.pro>
2021-08-12 17:54:24 +0300

    Add catalog for desktop

--- scripts/script.js
+++ scripts/script.js
@@ -2,7 +2,13 @@ $(document).ready(function () {
     // HEADER BURGER START
     $(".header__burger").click(function (event) {
         $(".header__burger, .header__nav, .header__nav-block").toggleClass('active');
-    })
+    });
 
     // HEADER BURGER END
+
+    //CATALOG TABS
+    $('.catalog__tab').click(function (event) {
+
+        $(".catalog__tab").toggleClass("active");
+    });
 });
