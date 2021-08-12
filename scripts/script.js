067a139f9eb22026cc6f4b022a17f887dff18346
Shabaykina <shabaykina@ivit.pro>
2021-08-12 13:02:59 +0300

    Add byn for banner + catalog structure

---
+++ scripts/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+    // HEADER BURGER START
+    $(".header__burger").click(function (event) {
+        $(".header__burger, .header__nav").toggleClass('active');
+    })
+
+    // HEADER BURGER END
+});
