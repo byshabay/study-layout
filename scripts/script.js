09337708fef623a236854fc0a0feb61505f42e8c
Shabaykina <shabaykina@ivit.pro>
2021-08-12 15:23:46 +0300

    Submenu for mobile

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
     // HEADER BURGER START
     $(".header__burger").click(function (event) {
-        $(".header__burger, .header__nav").toggleClass('active');
+        $(".header__burger, .header__nav, .header__nav-block").toggleClass('active');
     })
 
     // HEADER BURGER END
