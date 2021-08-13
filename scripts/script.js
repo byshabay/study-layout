7077dd5344fe9fbeb6af5e94c2d6067bb9dd8706
Shabaykina <shabaykina@ivit.pro>
2021-08-13 13:39:50 +0300

    Remade functionality of the catalog

--- scripts/script.js
+++ scripts/script.js
@@ -1,14 +1,28 @@
 $(document).ready(function () {
     // HEADER BURGER START
-    $(".header__burger").click(function (event) {
+    $(".header__burger").click(function (e) {
         $(".header__burger, .header__nav, .header__nav-block").toggleClass('active');
     });
-
     // HEADER BURGER END
 
-    //CATALOG TABS
-    $('.catalog__tab').click(function (event) {
-
-        $(".catalog__tab").toggleClass("active");
+    // HEADER BACKGROUND AT SCROLL START
+    $(document).scroll(function (e) {
+        $(window).scrollTop() > 100 ? $(".header__nav-block").addClass("scroll") : $(".header__nav-block").removeClass("scroll");
     });
+    // HEADER BACKGROUND AT SCROLL END
+
+    // CATALOG TABS START
+    var tab = $("#tabs .catalog__items > div");
+    tab.hide().filter(':first').show();
+
+    $(".catalog__tab a").click(function (e) {
+        tab.hide();
+        $(this.hash).show();
+        $(".catalog__tab a").removeClass("active-tab")
+        $(this).addClass("active-tab");
+    });
+    // CATALOG TABS END
+
+
+
 });
