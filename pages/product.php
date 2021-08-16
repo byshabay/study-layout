9d9aba0a4b8b736a216a3d11b05570864329a9f4
Shabaykina <shabaykina@ivit.pro>
2021-08-16 17:58:46 +0300

    Add product page structure

--- pages/product.php
+++ pages/product.php
@@ -14,10 +14,39 @@
     <script src="../scripts/script.js"></script>
 </head>
 <body>
-    <!-- 1. HEADER START -->
+    <!-- 1.HEADER START -->
     <?php
         include("header.php")
     ?>
-    <!-- 1. HEADER END -->
+    <!-- 1.HEADER END -->
+    <!-- 2.MAIN START -->
+    <main class="main">
+        <!-- 2.1.BANNER START -->
+        <section class="prod-banner lb-banner">
+             <h1>
+                <span class="lb-banner__our">
+                    product
+                </span>
+                <span>
+                    view
+                </span>
+            </h1>
+            <h2>
+                mens - casuals - hoodies & sweatshirts - <span>ave classic sweatshirt</span>
+            </h2>
+        </section>
+
+        <!-- 2.1.BANNER END -->
+        <!-- 2.2.PRODUCT INFO START -->
+            ИНВОРМАЦИЯ О ТОВАРЕ 
+        <!-- 2.2.PRODUCT INFO START -->
+
+    </main>
+    <!-- 2.MAIN END -->
+    <!-- 3.FOOTER START -->
+    <?php
+        include("footer.php");
+    ?>
+    <!-- 3.FOOTER END -->
 </body>
 </html>
