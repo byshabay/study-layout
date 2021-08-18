4b4abb7afa3d48089acc6eb52af3950217a9e046
Alyona Shabaykina <shabbaykina@mail.ru>
2021-08-18 14:12:34 +0300

    Change the layout method of the brand page

--- pages/brand.php
+++ pages/brand.php
@@ -42,26 +42,8 @@
 
         <!-- 2.2.CONTENT START -->
         <section class="content container">
-            <div class="content__column content__column-1">
-                <img class="content__img" src="../images/brand-1.png" alt="">
-                <img class="content__img" src="../images/brand-2.png" alt="">
-                <div>
-                    <span class="title-p">
-                        our values, vision and strategy
-
-                    </span>
-                    <p class="title-p content__title">
-                        The above image would be a great place for an advertising video
-                    </p>
-                    <p>
-                        Cras maximus venenatis malesuada. Nulla sagittis elit felis, ac facilisis quam ornare aliquam. Etiam cursus odio vitae dui dignissim, sed tempus lacus tincidunt et donec sapien velit, rhoncus eu nulla a, molestie tempus mi maecenas sagittis ornare.
-                        <br> Pellentesque sapien mi, <a href="#"> tincidunt nec magna vitae</a>, volutpat elementum odioni lorem Aliquam tempor massa vitae augue mattis tempor id in ante ut augue erat, luctus eil.
-
-                    </p>
-                </div>
-
-            </div>
-            <div class="content__column content__column-2">
+            <div class="content__row content__row-1">
+                <img class="content__img q1" src="../images/brand-1.png" alt="">
                 <div class="content__text-2">
                     <span class="title-p">
                         hand designed clothing
@@ -73,29 +55,49 @@
                         Praesent feugiat malesuada tristique maecenas rhoncus diam eget quam vestibulim consectetur, id condimentum leo porttitor. Cum sociis natoque penatibus eta magnis disut parturient montes, nascetur ridiculus mus. Donec sem lorem laoreet tempor un risus vitae, rutrum sodales nibh suspendisse congue metus nunc, id viverra elit loreti rhoncus quis consecteur es. Donec pulvinar tempor lorem a pretium justo interdum.
                     </p>
                 </div>
-                <img class="content__img" src="../images/brand-3.png" alt="">
-                <div class="content__text-3">
-                    <span class="title-p">
-                        Ethical trading
-                    </span>
-                    <p class="title-p content__title">
-                        we oversee the working conditions of the people in the supply chain
-                    </p>
-                    <p>
-                        Nullam dapibus consectetur neque, faucibus porttitor purus iaculis sed. Aenean eras dapibus augue, eget dignissim dui maecenas et rhoncus mim, vel semper arcu lorem
-                        Pellentesque congue justo esteir pellentesque aliquet massa eget posuere tincidunt.
-                        Cras viverra ullamcorper nunc accumsan hendrerit. <a href="#"> A link &rarr; </a>
-
-                    </p>
-                </div>
-                <div class="content-awards">
-                    <img src="../images/award-1.png" alt="" class="content__avard">
-                    <img src="../images/award-2.png" alt="" class="content__avard">
-                    <img src="../images/award-3.png" alt="" class="content__avard">
-                </div>
-
             </div>
+            <div class="content__columns">
+                <div class="content__column content__column-1">
+                    <img class="content__img q2" src="../images/brand-2.png" alt="">
+                    <div class="content__text-1">
+                        <span class="title-p">
+                            our values, vision and strategy
 
+                        </span>
+                        <p class="title-p content__title">
+                            The above image would be a great place for an advertising video
+                        </p>
+                        <p>
+                            Cras maximus venenatis malesuada. Nulla sagittis elit felis, ac facilisis quam ornare aliquam. Etiam cursus odio vitae dui dignissim, sed tempus lacus tincidunt et donec sapien velit, rhoncus eu nulla a, molestie tempus mi maecenas sagittis ornare.
+                            <br> Pellentesque sapien mi, <a href="#"> tincidunt nec magna vitae</a>, volutpat elementum odioni lorem Aliquam tempor massa vitae augue mattis tempor id in ante ut augue erat, luctus eil.
+
+                        </p>
+                    </div>
+                </div>
+                <div class="content__column content__column-2">
+
+                    <img class="content__img" src="../images/brand-3.png" alt="">
+                    <div class="content__text-3">
+                        <span class="title-p">
+                            Ethical trading
+                        </span>
+                        <p class="title-p content__title">
+                            we oversee the working conditions of the people in the supply chain
+                        </p>
+                        <p>
+                            Nullam dapibus consectetur neque, faucibus porttitor purus iaculis sed. Aenean eras dapibus augue, eget dignissim dui maecenas et rhoncus mim, vel semper arcu lorem
+                            Pellentesque congue justo esteir pellentesque aliquet massa eget posuere tincidunt.
+                            Cras viverra ullamcorper nunc accumsan hendrerit. <a href="#"> A link &rarr; </a>
+
+                        </p>
+                    </div>
+                    <div class="content-awards">
+                        <img src="../images/award-1.png" alt="" class="content__avard">
+                        <img src="../images/award-2.png" alt="" class="content__avard">
+                        <img src="../images/award-3.png" alt="" class="content__avard">
+                    </div>
+                </div>
+            </div>
         </section>
         <!-- 2.2.CONTENT END -->
 
