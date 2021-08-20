382de1fe3e28175317cc2719d7b2f69760ce331c
Alyona Shabaykina <shabbaykina@mail.ru>
2021-08-20 10:43:17 +0300

    Fix bugs with location-pin.png + bugs with style banner__btn

---
+++ scripts/Title.js
@@ -0,0 +1,23 @@
+class Title {
+
+
+    static function setTitle() {
+        let aTitle = [
+            {
+                title: 'testset'
+            },
+            {
+                title: 'testset'
+            },
+            {
+                title: 'testset'
+            },
+            {
+                title: 'testset'
+            }
+
+        ]
+
+        return aTitle;
+    }
+}
